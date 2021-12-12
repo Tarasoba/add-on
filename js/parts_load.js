ff8e0af964f6927ba63063ec82b3ca36ca53698d
tarasoba <80679108+Tarasoba@users.noreply.github.com>
2021-12-12 12:27:17 +0900

    Update parts_load.js

--- js/parts_load.js
+++ js/parts_load.js
@@ -1,4 +1,4 @@
 $(function () {
-    $("#header").load("../../parts/header.html");
-    $("#footer").load("../../parts/footer.html");
+    $("#header").load("../parts/header.html");
+    $("#footer").load("../parts/footer.html");
 });
