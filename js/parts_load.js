73dbe0e4134a463bf6b05d77ba0a0329dbd67789
tarasoba <80679108+Tarasoba@users.noreply.github.com>
2022-02-08 15:58:18 +0900

    Delete parts_load.js

--- js/parts_load.js
+++
@@ -1,5 +0,0 @@
-$(function () {
-    $("#header").load("https://tarasoba.github.io/add-on/parts/header.html");
-    $("#footer").load("https://tarasoba.github.io/add-on/parts/footer.html");
-    $("#top").load("https://tarasoba.github.io/add-on/parts/top.html");
-});
