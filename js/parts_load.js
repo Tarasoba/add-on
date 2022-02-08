0064655eb68cf37af949dbc6104f6e39a463e867
tarasoba <80679108+Tarasoba@users.noreply.github.com>
2022-02-08 15:47:08 +0900

    Add files via upload

--- js/parts_load.js
+++ js/parts_load.js
@@ -1,4 +1,5 @@
 $(function () {
     $("#header").load("https://tarasoba.github.io/add-on/parts/header.html");
     $("#footer").load("https://tarasoba.github.io/add-on/parts/footer.html");
+    $("#top").load("https://tarasoba.github.io/add-on/parts/top.html");
 });
