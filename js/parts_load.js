a9a3c4e9586bec3f84d5ee99b0e0498bd8b5b5ce
tarasoba <80679108+Tarasoba@users.noreply.github.com>
2022-02-13 00:51:52 +0900

    Add files via upload

---
+++ js/parts_load.js
@@ -0,0 +1,6 @@
+$(function () {
+    $("#header").load("https://tarasoba.github.io/add-on/parts/header.html");
+    $("#footer").load("https://tarasoba.github.io/add-on/parts/footer.html");
+    $("#top").load("https://tarasoba.github.io/add-on/parts/top.html");
+});
+//謎のJS（未使用）
